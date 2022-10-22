03ed400663a864089a3878517812cf4664a37c05
Praise Jones <praisejones18@gmail.com>
2022-10-22 13:53:26 +0100

    final update....... I guess...

--- First-Sever/server.js
+++ First-Sever/server.js
@@ -2,12 +2,20 @@ const http = require('http');
 
 const server = http.createServer(function(req, res){
     //Headers
-    res.writeHead(200, { 'content-Type': 'text/plain'})
+    res.writeHead(200, { 'content-Type': 'text/html'})
 
     //send back some information
-    res.end("This is my first node sever")
+    // res.end("This is my first node sever")
+    res.end(`
+        <html>
+        <body style="background-color: black; color: white; text-align: center;">
+            <h1>Hey Server</h1>
+            <p>Hello world it's my first server.</p>
+        </body>
+        </html>
+    `);
 });
 
-server.listen(3000, '127.0.0.1')
+server.listen(7000, '127.0.0.1')
 
 console.log("Hurray, I just created a server!")
