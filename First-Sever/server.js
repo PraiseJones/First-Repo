c69db610acb910ff8eee296947aab89a4ab6b2e2
Praise Jones <praisejones18@gmail.com>
2022-10-22 13:52:44 +0100

    updated

--- First-Sever/server.js
+++ First-Sever/server.js
@@ -1,5 +1,13 @@
 const http = require('http');
 
-http.createServer(function(req, res){
+const server = http.createServer(function(req, res){
+    //Headers
+    res.writeHead(200, { 'content-Type': 'text/plain'})
 
+    //send back some information
+    res.end("This is my first node sever")
 });
+
+server.listen(3000, '127.0.0.1')
+
+console.log("Hurray, I just created a server!")
